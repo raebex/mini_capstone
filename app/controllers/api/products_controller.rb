7e5b5b8a0b525ae2b4c1a8dee0419650541b6392
rebecca <r.young1025@gmail.com>
2021-01-13 14:21:25 -0600

    add route to return first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def first
+    @product = Product.first
+    render "first_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products" => "products#all"
+    get "/first_product" => "products#first"
   end
 end
