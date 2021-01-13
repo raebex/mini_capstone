1c94f1f674fd3ccadcd0a69b04dd802afee7ac8b
rebecca <r.young1025@gmail.com>
2021-01-13 14:12:00 -0600

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  [
+    {
+      name: "Envy - Definition Of Impossibility",
+      price: 74,
+      image_url: "https://img.discogs.com/UYS7ad5wPo0XYP5V148hyca7iXc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-14522556-1576921801-1382.jpeg.jpg",
+      description: "Vinyl, 10inch, 45 RPM, EP, Stereo, Black-In-Ultraclear"
+    },
+    {
+      name: "Holy Fawn - Death Spells",
+      price: 38,
+      image_url: "https://img.discogs.com/qgI9ANKJhGnOI0eUa7hksYzJ7CY=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13370925-1552933360-9623.jpeg.jpg",
+      description: "2 × Vinyl, LP, Album, Limited Edition, Stereo, Clear"
+    },
+    {
+      name: "We Were Skeletons ‎– Blame & Aging",
+      price: 14,
+      image_url: "https://img.discogs.com/gRM13UI33JAKJzgiirwnJh7e4aQ=/fit-in/225x225/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4031461-1352947201-4912.jpeg.jpg",
+      description: "Vinyl, LP, Red/Orange w/ Black Splatter (out of 200)"
+    },
+    {
+      name: "Birds In Row - We Already Lost The World",
+      price: 20,
+      image_url: "https://img.discogs.com/MG_jraGSnShUP3TELaXTVP1qyGc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-12313663-1532731828-1613.png.jpg",
+      description: "Vinyl, LP, Album, Limited Edition, Repress, Black and Transparent Blue"
+    },
+    {
+      name: "Gouge Away - Swallow b/w Sweat",
+      price: 8,
+      image_url: "https://img.discogs.com/lvd7f4IkTcQa7qvHTQe3HkHh134=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13486991-1555145287-3564.jpeg.jpg",
+      description: "Vinyl, 7inch, Single, Limited Edition, Repress, Purple Marble"
+    }
+  ]
+)
