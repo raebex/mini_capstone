582b57d45202202ebb34dc1845ad5a0543fbb2d0
rebecca <r.young1025@gmail.com>
2021-01-13 14:33:28 -0600

    use tty-table gem to display products in a table

--- script.rb
+++ script.rb
@@ -1,5 +1,11 @@
 require "http"
+require "tty-table"
 
 response = HTTP.get("http://localhost:3000/api/all_products")
 products = response.parse
-pp products
+#pp products
+
+products_array = products.map {|product| product.values}
+
+table = TTY::Table.new(["id", "name", "price", "image_url", "description"], products_array)
+puts table.render(:ascii)
