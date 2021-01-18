7678d585442de5cb5991d00cdb5f41cefd6168e9
rebecca <r.young1025@gmail.com>
2021-01-18 12:35:41 -0600

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,5 @@ Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
-    
-    # get "/first_product" => "products#first"
-    # get "/second_product" => "products#second"
-    # get "/single_product_query" => "products#single_product"
-    # get "/single_product_url/:id" => "products#single_product"
-    # post "/single_product_body" => "products#single_product"
   end
 end
