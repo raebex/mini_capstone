9ff49523cd732d8db457e957ab6121cbdbc5fe2d
rebecca <r.young1025@gmail.com>
2021-01-19 12:56:18 -0600

    allow cors for frontend app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  # From Actualize
   protect_from_forgery with: :exception, if: -> { request.format.html? }
+  # protect_from_forgery with: :null_session
 end
