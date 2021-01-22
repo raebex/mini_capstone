c6733fb0fbeaf453549a3eab307b3be464b88d13
rebecca <r.young1025@gmail.com>
2021-01-22 14:14:10 -0600

    add active record queries

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,19 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    if params[:discount]
+      @products = @products.where("price < ?", 10)
+    end
+
+    if params[:sort] == "price" && params[:sort_order]
+      @products = @products.order(:price => params[:sort_order])
+    end
+
+    if params[:search]
+      @products = @products.where("name iLIKE ? OR description iLIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
     render "index.json.jb"
   end
 
